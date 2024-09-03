9e586e89952b3eab0e62b35159c539351be1fc57
HyeonjinChoi <eaglushyeonjin@gmail.com>
2024-09-03 14:40:22 +0900

    Init

---
+++ src/main/java/com/sungkyunkwan/tload/TLoadApplication.java
@@ -0,0 +1,13 @@
+package com.sungkyunkwan.tload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TLoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungkyunkwan/tload/TLoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungkyunkwan.tload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TLoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

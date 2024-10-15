3de4bf08aa1e35a6281902a8669217ef8346181a
TaeUk471 <asky990223@gmail.com>
2024-10-15 10:36:11 +0900

    Feat: tailwind css 적용 및 확인

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 // app/layout.tsx
 import { ReactNode } from "react";
 
+import "../styles/global.css";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">

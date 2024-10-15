c601ec59e0be8d742f9d94bc0a16d97cd6532e47
TaeUk471 <asky990223@gmail.com>
2024-10-15 09:51:02 +0900

    Feat: Layout 컴포넌트 기본 설정

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,20 @@
+// app/layout.tsx
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-
-
-        
-      </body>
+    <html lang="ko">
+      <head>
+        <title>T-load</title>
+        <meta name="description" content="This is an awesome website about trip." />
+        <meta name="keywords" content="trip, tour, tourist, communication" />
+        <meta property="og:title" content="T-Load" />
+        <meta property="og:description" content="This is an awesome website about trip." />
+        <meta property="og:image" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }

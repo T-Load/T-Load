9fe1358ba0cfb306ff763da8ff6073be4ecd83d8
HanWol_23 <90249043+TaeUk471@users.noreply.github.com>
2024-10-17 11:29:22 +0900

    Revert "Auth 페이지 제작 (로그인 및 회원가입)"

--- frontend/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function AuthLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="flex justify-center items-center w-full h-screen bg-gradient-to-b from-blue-400 to-pink-300">
-      {children}
-    </div>
-  );
-}
--- frontend/constant/AuthInputProps.ts
+++
@@ -1,8 +0,0 @@
-export interface AuthInputProps {
-  label: string;
-  name: string;
-  type: "email" | "password" | "text";
-  placeholder: string;
-  value: string;
-  onChange: (value: string) => void;
-}

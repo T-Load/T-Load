05260fbe7dc0698cb233286afd55ad795a4e7b9c
TaeUk471 <asky990223@gmail.com>
2024-10-23 15:09:57 +0900

    Feat: Button 컴포넌트 제작

--- frontend/components/button/Button.tsx
+++ frontend/components/button/Button.tsx
@@ -1,9 +1,24 @@
+import { buttonColor, buttonColorType, buttonSize, buttonSizeType } from "types/button";
+
 interface ButtonProps {
+  size: buttonSizeType;
+  color: buttonColorType;
+  isLoading?: boolean;
   children: React.ReactNode;
 }
 
-const Button = ({ children }: ButtonProps) => {
-  return <button className="flex-center">{children}</button>;
+const Button = ({ size, color, isLoading, children, ...rest }: ButtonProps) => {
+  const sizeClass = buttonSize[size];
+  const colorClass = buttonColor[color];
+
+  return (
+    <>
+      <button className={`flex-center ${sizeClass} ${colorClass}`} disabled={isLoading} {...rest}>
+        {" "}
+        {isLoading ? "Loading.." : children}
+      </button>
+    </>
+  );
 };
 
 export default Button;

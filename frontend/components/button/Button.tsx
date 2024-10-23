8aa0285aa0d0d160f9daac953fffd7cc72317ae2
TaeUk471 <asky990223@gmail.com>
2024-10-23 16:24:19 +0900

    Feat: Pagenation 제작 완료

--- frontend/components/button/Button.tsx
+++ frontend/components/button/Button.tsx
@@ -5,15 +5,16 @@ interface ButtonProps {
   color: buttonColorType;
   isLoading?: boolean;
   children: React.ReactNode;
+  onClick: () => void;
 }
 
-const Button = ({ size, color, isLoading, children, ...rest }: ButtonProps) => {
+const Button = ({ size, color, isLoading, children, onClick, ...rest }: ButtonProps) => {
   const sizeClass = buttonSize[size];
   const colorClass = buttonColor[color];
 
   return (
     <>
-      <button className={`flex-center ${sizeClass} ${colorClass}`} disabled={isLoading} {...rest}>
+      <button className={`flex-center ${sizeClass} ${colorClass}`} disabled={isLoading} onClick={onClick} {...rest}>
         {" "}
         {isLoading ? "Loading.." : children}
       </button>

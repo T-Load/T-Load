cd5e2e037079fc35ac1ccb5160d4a7acce74ab05
TaeUk471 <asky990223@gmail.com>
2024-10-24 10:56:16 +0900

    Feat: MainPage의 SSR을 위해 클라이언트 컴포넌트 최소화

--- frontend/app/(app)/page.tsx
+++ frontend/app/(app)/page.tsx
@@ -1,42 +1,6 @@
-"use client";
-
 import MainForm from "@components/common/MainForm";
-import { useEffect } from "react";
 
 export default function MainPage() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = "https://fd82-1-237-90-238.ngrok-free.app/api/users/profile/1";
-
-      // 헤더 설정
-      const headers = {
-        "Content-Type": "application/json",
-        "ngrok-skip-browser-warning": "69420",
-      };
-
-      // 요청 옵션 설정
-      const options = {
-        method: "GET",
-        headers: headers,
-      };
-
-      try {
-        const response = await fetch(url, options);
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Fetch error:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <>
       <MainForm />
